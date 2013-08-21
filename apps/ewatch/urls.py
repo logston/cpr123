bdf590746900a7ed9b9487306f3e6b7273f79496
logston <paul.logston@gmail.com>
2013-08-21 02:39:00 +0000

    Added a view to watch details of scrape.

--- apps/ewatch/urls.py
+++ apps/ewatch/urls.py
@@ -5,4 +5,5 @@ from apps.ewatch import views
 urlpatterns = patterns('',
         url(r'^$', views.index),
         url(r'^list_classes/$', views.list_classes),
+        url(r'^scrape_details/$', views.scrape_details),
         )
--- apps/ewatch/views.py
+++ apps/ewatch/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render_to_response
 
-from apps.ewatch.models import Class, Registration
+from apps.ewatch.models import Class, Registration, UpdateCheckClass
 
 def index(request):
     return render_to_response('ewatch/base.html')
+
+def scrape_details(request):
+    tdata = {}
+    scraped = UpdateCheckClass.objects.values_list('class_pk', flat=True)
+    tdata['classes_scraped'] = len(set(scraped))
+    tdata['total_classes'] = Class.objects.all().count()
+    tdata['percent_classes_scraped'] = round((tdata['classes_scraped']/ \
+            tdata['total_classes'])*100)
+    return render_to_response('ewatch/scrape_details.html', tdata)
 
 def list_classes(request):
     clist = Class.objects.all().order_by('-enrollware_id')
