047beb849699250d1003968e57d2a01a73ebe55e
logston <paul.logston@gmail.com>
2013-08-22 17:14:15 +0000

    Updated templates to include bootstrap.js

--- apps/ewatch/views.py
+++ apps/ewatch/views.py
@@ -7,22 +7,19 @@ def index(request):
 
 def scrape_details(request):
     tdata = {}
+    # percent of past classes scraped
     scraped_c = UpdateCheckClass.objects.values_list('class_pk', flat=True)
-    scraped_r = RegistrationCheckClass.objects.
-    
-
     tdata['classes_scraped'] = len(set(scraped_c))
     tdata['total_classes'] = Class.objects.all().count()
     tdata['percent_classes_scraped'] = round((tdata['classes_scraped']/ \
             tdata['total_classes'])*100)
+    # percent of class scrapes succuessful (ie. has course name)
+    
+    # percent of registrations scrapes sucuessful (ie. has name)
     
     return render_to_response('ewatch/scrape_details.html', tdata)
 
 def list_classes(request):
-    clist = Class.objects.all().order_by('-enrollware_id')
     tdata = {'classes': []}
-    for c in clist:
-        r = Registration.objects.filter(class_pk=c).count()
-        tdata['classes'].append((c, r))
     
     return render_to_response('ewatch/list_classes.html', tdata)
