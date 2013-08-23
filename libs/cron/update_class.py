a7f5b34c3072166ed95d1ec36f0d3185468483dc
Paul Logston <code@logston.me>
2013-08-23 19:39:38 -0400

    Removed deteled files from repo

--- libs/cron/update_class.py
+++
@@ -1,4 +0,0 @@
-
-from apps.ewatch.models import *
-
-from libs.fetch.test_clas
--- tests/us_states.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from libs.us_states import us_states
-
-class TestUSStatesModule(TestCase)
-    def test_number_of_states(self):
-        """Number of states plus DC"""
-        self.assertEqual(len(us_states), 51)
